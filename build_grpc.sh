7920692895f4c8b6371e7eabafccc74c866c193d
Sumant Tambe <sutambe@yahoo.com>
2018-09-28 14:21:36 -0700

    update gprc build

--- build_grpc.sh
+++ build_grpc.sh
@@ -12,7 +12,29 @@ function build_grpc () {
   cd ../..
 }
 
+function travis_build () {
+  if [ ! -d "travis_build" ]; then
+   mkdir travis_build
+  fi
+  cd travis_build
+  cmake -DFLATBUFFERS_BUILD_GRPCTEST=ON -DGRPC_INSTALL_PATH=${GRPC_INSTALL_PATH} -DPROTOBUF_DOWNLOAD_PATH=${PROTOBUF_DOWNLOAD_PATH} ..
+  make
+  if [ "$OSTYPE" = "linux-gnu" ]
+  then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${GRPC_INSTALL_PATH}/lib
+    make test ARGS=-V
+    # ./grpctest
+  elif [ "$OSTYPE" = "darwin17" ]
+  then
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${GRPC_INSTALL_PATH}/lib 
+    make test ARGS=-V
+    # ./grpctest
+  fi
+  cd ..
+}
+
 GRPC_INSTALL_PATH=`pwd`/google/grpc/install
 PROTOBUF_DOWNLOAD_PATH=`pwd`/google/grpc/third_party/protobuf
 
 build_grpc
+#travis_build
